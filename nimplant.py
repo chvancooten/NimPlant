85cc5afdfb646d53c5a3ac545dd77a5489f8ce3e
chvancooten <chvancooten@gmail.com>
2024-06-16 18:51:26 +0200

    Update CI/CD

--- nimplant.py
+++ nimplant.py
@@ -16,8 +16,6 @@ import time
 import toml
 from client.dist.srdi.ShellcodeRDI import ConvertToShellcode, HashFunctionName
 
-# TODO: Fix CI/CD paths: https://github.com/chvancooten/NimPlant-private/actions/runs/9530913515/job/26271309362
-
 
 def print_banner():
     """Print the NimPlant banner."""
@@ -139,12 +137,14 @@ def compile_implant(implant_type, binary_type, xor_key):
         # Compile all
         print(f"Compiling .exe for {message}")
         compile_function("exe", xor_key, config)
-        print(f"Compiling self-deleting .exe for {message}")
-        compile_function("exe-selfdelete", xor_key, config)
         print(f"Compiling .dll for {message}")
         compile_function("dll", xor_key, config)
         print(f"Compiling .bin for {message}")
         compile_function("raw", xor_key, config)
+        # TODO: Exe-Selfdelete
+        if implant_type != "rust":
+            print(f"Compiling self-deleting .exe for {message}")
+            compile_function("exe-selfdelete", xor_key, config)
 
 
 def compile_nim_debug(binary_type, xor_key, config):
@@ -256,8 +256,6 @@ def compile_rust(binary_type, xor_key, config, debug=False):
             target_path = target_path + "nimplant.exe"
             compile_command = compile_command + " --bin=nimplant"
         case "exe-selfdelete":
-            # TODO: Exe-Selfdelete
-            # TODO: Add to CI/CD as well
             print("RUST EXE-SELFDELETE NOT YET IMPLEMENTED.")
             exit(1)
         case "dll":

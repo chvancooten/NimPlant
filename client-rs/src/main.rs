f3d6debfcbddce2809e689f7a62fe98a2af8b89f
Cas van Cooten <chvancooten@gmail.com>
2024-06-16 22:53:20 +0200

    Fix unused import error for non-selfdelete

--- client-rs/src/main.rs
+++ client-rs/src/main.rs
@@ -7,16 +7,14 @@
 
 mod app;
 
-use app::debug::{allocate_console_debug_only, debug_println};
-
 fn main() {
     // Allocate a console if we're in debug mode
-    allocate_console_debug_only();
+    app::debug::allocate_console_debug_only();
 
     // Self-delete the binary if the feature is enabled
     #[cfg(feature = "selfdelete")]
-    if let Err(_e) = crate::app::self_delete::perform() {
-        debug_println!("Failed to self-delete: {:?}", _e);
+    if let Err(_e) = app::self_delete::perform() {
+        app::debug::debug_println!("Failed to self-delete: {:?}", _e);
     };
 
     app::main();
